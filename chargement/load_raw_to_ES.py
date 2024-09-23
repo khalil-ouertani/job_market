c1a7649050db2f9ac9d1a0b0e72612caf34de5e3
Khalil <khalilouertani@gmail.com>
2024-09-23 20:40:00 +0000

    dockerisation du chargement + elasticsearch + kibana

--- chargement/load_raw_to_ES.py
+++ chargement/load_raw_to_ES.py
@@ -1,10 +1,21 @@
 import json
+import time
 from elasticsearch import Elasticsearch, helpers
 
-es = Elasticsearch([{'host': 'localhost', 'port': 9200, 'scheme': 'http'}])
+es = None
+retries = 10  # Nombre d'essais avant d'abandonner
 
-if not es.ping():
-    raise ValueError("Connexion à ElasticSearch échouée")
+for i in range(retries):
+    try:
+        es = Elasticsearch(['http://es-container:9200'])
+        if es.ping():
+            print("Connexion réussie à ElasticSearch")
+            break
+    except Exception as e:
+        print(f"Essai {i+1}/{retries} : Connexion échouée, nouvelle tentative dans 20 secondes...")
+        time.sleep(20)
+else:
+    raise ValueError("Impossible de se connecter à ElasticSearch après plusieurs essais")
 
 def load_json_to_elasticsearch(json_file_path, index_name):
     with open(json_file_path, 'r', encoding='utf-8') as file:
@@ -21,7 +32,7 @@ def load_json_to_elasticsearch(json_file_path, index_name):
     print(f"Chargement terminé pour l'index {index_name}")
 
 # Charger le fichier Indeed dans ElasticSearch
-load_json_to_elasticsearch('/home/ubuntu/job_market/data/indeed.json', 'data_jobs_raw_indeed') #insérer votre file_path
+load_json_to_elasticsearch('/app/indeed.json', 'data_jobs_raw_indeed') #insérer votre file_path
 
 # Charger le fichier FranceTravail dans ElasticSearch
-load_json_to_elasticsearch('/home/ubuntu/job_market/data/franceTravail.json', 'data_jobs_raw_francetravail') #insérer votre file_path
+load_json_to_elasticsearch('/app/franceTravail.json', 'data_jobs_raw_francetravail') #insérer votre file_path
